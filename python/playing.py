1e0c03d728d147e5589c380a97d9ce4f64e180c2
Angelochen65 <158617036+Angelochen65@users.noreply.github.com>
2024-05-31 15:27:43 -0400

    Update playing.py

--- python/playing.py
+++ python/playing.py
@@ -3,3 +3,26 @@ print('Content-type: text/html\n')
 
 import cgi
 data = cgi.FieldStorage()
+
+HTML_HEADER = """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+"""
+
+HTML_FOOTER = """
+</body>
+</html>
+"""
+
+name = "Blue"
+if ('name' in data):
+    name = data['name'].value
+bgcolor = 'Blue'
+if ('bgcolor' in data):
+    bgcolor = data['bgcolor'].value
+print(data)
