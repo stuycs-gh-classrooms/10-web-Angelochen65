f3ef5fa022829b3b51b69b5321a54cc269008a06
Angelochen65 <158617036+Angelochen65@users.noreply.github.com>
2024-06-03 15:27:22 -0400

    Update p0.py

--- python/p0.py
+++ python/p0.py
@@ -25,7 +25,7 @@ if ('name' in data):
     name = data['name'].value
 
 html= HTML_HEADER
-html+= '<p> Hello there ' + name + "! Sadly I think that you're stuck here. There's a weird looking door thing if you want to try to go through it. (I'd kind of forcing you to go, or else we'll be stuck here for a while) </p>"
+html+= '<p> Hello there ' + name + "! Sadly I think that you're stuck here. There's a weird looking door thing if you want to try to go through it. (I'm kind of forcing you to go, or else we'll be stuck here for a while) </p>"
 html+= '<br> <a href = "p1.html">' +  "Let's go I guess.. </a>"
 html+= HTML_FOOTER
 print(html)
