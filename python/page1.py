e05e55bdc39e5ded6f0e45ceb590b4e6def3f988
Angelochen65 <158617036+Angelochen65@users.noreply.github.com>
2024-06-05 15:21:59 -0400

    Update page1.py

--- python/page1.py
+++ python/page1.py
@@ -18,7 +18,17 @@ HTML_FOOTER = """
 </body>
 </html>
 """
-
+RETURN = """
+<body>
+  <t1> There seem to be some weird shapes on the wall... and a lock? Figure out the passcode and access the next door. </t1>
+    <p>(Insert images of hexagon, star, square, triangle)</p>
+    <form action = "page1.py" method = "GET">
+    
+      <input type = "text" name = "passcode" value = "0000">
+      <br>
+      <input type = "submit" name = "Submit">
+    </form>
+    """
 
 passcode = '0000'
 if ('passcode' in data):
@@ -30,6 +40,7 @@ else:
     msg = "<p> Try Again </p>"
 
 html= HTML_HEADER
+html+= RETURN
 html+= msg
 html+= HTML_FOOTER
 print(html)
